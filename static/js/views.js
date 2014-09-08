e21cf02e23f390f4d12153965e2c3444f3966a05
Eugene Zagidullin <e.asphyx@gmail.com>
2014-09-08 15:51:56 +0300

    More work

--- static/js/views.js
+++ static/js/views.js
@@ -26,10 +26,21 @@ window.Controllers = window.Controllers || {};
 })();
 
 $(document).ready(function(){
-	Backbone.history.start({pushState: true});
-	if(window.location.hash == '#_=_') {
-		Backbone.history.navigate(window.location.pathname, {trigger: false, replace: true});
+    if(window.location.hash && window.location.hash == '#_=_') {
+        if(window.history && history.pushState) {
+            window.history.pushState("", document.title, window.location.pathname);
+        } else {
+            // Prevent scrolling by storing the page's current scroll offset
+            var scroll = {
+                top: document.body.scrollTop,
+                left: document.body.scrollLeft
+            };
+            window.location.hash = '';
+            // Restore the scroll offset, should be flicker free
+            document.body.scrollTop = scroll.top;
+			document.body.scrollLeft = scroll.left;
+    	}
     }
-
+    
 	window.userProfile = new Controllers.UserProfile(window.AuthData.ID);
 });
